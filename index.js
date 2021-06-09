49239ddaca1e10cfe01558c0098421e627872c04
Aric McAlister <aricmca@gmail.com>
2021-06-09 14:45:56 -0500

    commit tree bc442b92b11b1b36aea8a2deb03406e0d50f95d1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(num1) {
+    if (num1 < 42) {
+        return (42 - num1)
+    }
+    return (num1 - 42);
+}
+
+function distanceFromHqInFeet(num1) {
+    return distanceFromHqInBlocks(num1) * 264;
+}
+
+function distanceTravelledInFeet(num1,num2) {
+    if (num1 < num2) {
+        return ((num2 - num1) * 264)
+    }
+    return ((num1 - num2) * 264)
+}
+
+function calculatesFarePrice(start,destination) {
+    if (((start - destination) * 264) <= 400) {
+        return 0;
+    }
+    else if (((start - destination) * 264) > 400 && ((start - destination) * 264) < 2000) {
+        return ((((start - destination) * 264) - 400) * .02);
+    }
+    else if (((start - destination) * 264) > 2000 && ((start - destination) * 264) < 2500) {
+        return 25;
+    } 
+    else if (((start - destination) * 264) >= 2500) {
+        return 'cannot travel that far';
+    }
+}
